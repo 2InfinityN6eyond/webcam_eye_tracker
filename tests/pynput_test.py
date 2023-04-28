0e7bda82a3f7ee3210059f890f85054fc575bf36
2InfinityN6eyond <hjp1nskku@g.skku.edu>
2023-04-28 14:24:16 +0900

    sdf

--- tests/pynput_test.py
+++ tests/pynput_test.py
@@ -5,7 +5,7 @@ controller = Controller()
 
 
 for i in range(1000) :
-    #controller.position = (i, i)
+    controller.position = (i, i)
 
-    controller.move(-1, -1)
+    #controller.move(-1, -1)
     time.sleep(0.01)
